75feb760255fe250263d494d2bfb28353f66afbc
maciejmatu <maciek.matuszewski@gmail.com>
2019-08-04 23:31:13 +0200

    Remove dependency on external api delay proxy

--- pages/api/time.js
+++ pages/api/time.js
@@ -10,9 +10,9 @@ const sleep = time => new Promise(resolve => setTimeout(resolve, time));
 async function Endpoint(req, res) {
   const { delay, location } = req.query;
 
-  await sleep(15);
+  await sleep(delay);
   const data = await fetch(
-    `http://slowwly.robertomurray.co.uk/delay/${delay}/url/https://worldtimeapi.org/api/timezone/${location}`
+    `https://worldtimeapi.org/api/timezone/${location}`
   ).then(res => res.json());
 
   res.json(data);
